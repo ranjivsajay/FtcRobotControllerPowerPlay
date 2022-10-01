6a7734365b8b9f9a549b8409b39c63dfa2064f08
ranjivsajay <64162329+ranjivsajay@users.noreply.github.com>
2022-10-01 12:30:35 -0400

    UpliftRobot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/Core/UpliftRobot.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Core.Core;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class UpliftRobot {
+    DcMotor leftFront, rightFront, leftBack, rightBack;
+
+    public LinearOpMode opMode;
+    public HardwareMap hardwareMap;
+
+    public UpliftRobot(LinearOpMode opMode) {
+        this.opMode = opMode;
+        getHardware();
+    }
+
+    public void getHardware() {
+        hardwareMap = opMode.hardwareMap;
+
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+    }
+
+    public DcMotor getLeftFront() {
+        return leftFront;
+    }
+
+    public DcMotor getLeftBack() {
+        return leftBack;
+    }
+
+    public DcMotor getRightBack() {
+        return rightBack;
+    }
+
+    public DcMotor getRightFront() {
+        return rightFront;
+    }
+}
